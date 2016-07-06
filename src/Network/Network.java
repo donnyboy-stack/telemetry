05e843bcab85a53c344b6c4489b986ac0cd16873
alecgunnar <alecgunnar@gmail.com>
2016-07-06 10:01:10 -0400

    Style updates

--- src/Network/Network.java
+++ src/Network/Network.java
@@ -18,18 +18,15 @@ class Network implements NetworkInterface {
     }
 
     public void request (String channel, NetworkSubscriberInterface subscriber) {
-        if (!subscribers.containsKey(channel)) {
+        if (!subscribers.containsKey(channel))
             subscribers.put(channel, new ArrayList<NetworkSubscriberInterface>());
-        }
 
         subscribers.get(channel).add(subscriber);
     }
 
     public void transmit (String channel, Object data) {
-        if (subscribers.containsKey(channel)) {
-            for (NetworkSubscriberInterface subscriber : subscribers.get(channel)) {
+        if (subscribers.containsKey(channel))
+            for (NetworkSubscriberInterface subscriber : subscribers.get(channel))
                 subscriber.receive(channel, data);
-            }
-        }
     }
 }
