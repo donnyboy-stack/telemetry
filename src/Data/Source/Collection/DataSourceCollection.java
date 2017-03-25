0a7759c2dcd3d106a54ae7295802b2013563b692
alecgunnar <alecgunnar@gmail.com>
2017-03-25 15:05:07 -0400

    Added collection back

---
+++ src/Data/Source/Collection/DataSourceCollection.java
@@ -0,0 +1,14 @@
+/**
+ * Sunseeker Telemetry
+ *
+ * @author Alec Carpenter <alecgunnar@gmail.com>
+ * @date July 9, 2016
+ */
+
+package sunseeker.telemetry;
+
+import java.util.HashMap;
+
+class DataSourceCollection extends HashMap<String, DataSourceInterface> implements DataSourceCollectionInterface {
+
+}
